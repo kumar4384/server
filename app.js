654b50fab3e29740a0071f47b1b5d344cdd6dc43
kumar4384 <kumar4384@gmail.com>
2023-08-15 07:52:54 +0530

    mogodb con string change

--- app.js
+++ app.js
@@ -3,30 +3,28 @@
 //mongodb+srv://tn-school-audit-admin:Mongodbadmin12@cluster0.807ootn.mongodb.net/?retryWrites=true&w=majority
 
 import express from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import auditRouter from "./routes/audit-routes";
 import cors from "cors";
 
 const app = express();
 
-app.listen(3000, () => console.log("App is listening"));
-// console.log("starting.....");
-// let corsOptions = {
-//   origin: "http://localhost:4200",
-//   optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
+let corsOptions = {
+  origin: "http://localhost:4200",
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-// app.use(express.json());
-// app.use("/audit", auditRouter);
+app.use(express.json());
+app.use("/audit", auditRouter);
 
-// mongoose.connect("mongodb://localhost/tn-school-audit")
-// .then(() => console.log("DB connection successful"))
-// .catch((error) => console.log("error while connecting DB===>"+error));
+mongoose.connect("mongodb://localhost/tn-school-audit")
+.then(() => console.log("DB connection successful"))
+.catch((error) => console.log("error while connecting DB===>"+error));
 
-// app.listen(3000, '127.0.0.1', () => {
-//   console.log("server is up");
-// });
+app.listen(3000, () => {
+  console.log("server is up");
+});
 
 // mongoose
 //   .connect(
